a531172f9de8f2e49867d75929409a7e8cc46619
forestus <guilhermepm164@gmail.com>
2021-07-11 17:47:34 -0300

    feat: add user router

---
+++ src/routes/user.ts
@@ -0,0 +1,18 @@
+import UserController from '@controllers/UserController'
+import UserValidator from '@services/User/UserValidator'
+import { Router } from 'express'
+import { ensureAuthenticated } from '@middlewares/ensureAuthenticated'
+const router = Router()
+
+// Rotas Publicas
+router.post('/signin', UserValidator.validateLogin, UserController.signin)
+router.post('/', UserValidator.validateStore, UserController.store)
+// Autenticação de Usuário
+router.use(ensureAuthenticated)
+// Rotas Privadas
+router.get('/', UserController.findAll)
+router.get('/:id', UserValidator.validateId, UserController.findOne)
+router.put('/:id', UserValidator.validateUpdate, UserController.update)
+router.delete('/:id', UserValidator.validateId, UserController.destroy)
+
+export default router
